db6024ba2cb345eaaac7237601e790fea4cb45c5
Harsh Raundal <dkingston0000@gmail.com>
2026-05-10 16:22:49 +0530

    Create basics revision.py

---
+++ basics revision.py
@@ -0,0 +1,9 @@
+name = input("Enter the name here:")
+
+if "Mr" in name:
+    print("You are a man, married man !")
+
+elif "Mrs" in name:
+    print("You are a woman, married woman !")
+else:
+    print("You need to have at least one initial like mr or mrs!")
